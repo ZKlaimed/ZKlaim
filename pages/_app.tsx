e0b792e29ae7ec0441a24b8e10b5682a2575c041
“Marcus” <115784464+Marcussy34@users.noreply.github.com>
2026-01-30 17:59:25 +0800

    wrap application with wallet provider to enable wallet connectivity throughout the app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,15 @@
 import type { AppProps } from 'next/app';
 import '@/styles/globals.css';
+import { AleoWalletProvider } from '@/components/providers/wallet-provider';
 
 /**
  * ZKLAIM Application Root
  * Wraps all pages with global providers and styles
  */
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <AleoWalletProvider>
+      <Component {...pageProps} />
+    </AleoWalletProvider>
+  );
 }
